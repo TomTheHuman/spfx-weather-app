839df0d81413bacad7bbe8bf6625a4312c3ffce5
tomthehuman <grimmm94@gmail.com>
2021-01-22 16:42:50 -0800

    Removed default SCC properties Fixed column resizing Implemented navigator.geolocation in WeatherApp.txs
    Need to use Google geocoding API to get city and state name

--- src/webparts/weatherApp/components/WeatherApp.tsx
+++ src/webparts/weatherApp/components/WeatherApp.tsx
@@ -4,19 +4,22 @@ import { IWeatherAppProps } from "./IWeatherAppProps";
 import { escape } from "@microsoft/sp-lodash-subset";
 
 export default class WeatherApp extends React.Component<IWeatherAppProps, {}> {
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Position is: ", position);
+    });
+  }
+
   public render(): React.ReactElement<IWeatherAppProps> {
     return (
       <div className={styles.weatherApp}>
         <div className={styles.container}>
           <div className={styles.flex}>
-            <div className={styles.column}>
+            <div className={styles.columnSm}>
               <h1>Column 1</h1>
             </div>
-            <div className={styles.column}>
+            <div className={styles.columnMed}>
               <h1>Column 2</h1>
-            </div>
-            <div className={styles.column}>
-              <h1>Column 3</h1>
             </div>
           </div>
         </div>
