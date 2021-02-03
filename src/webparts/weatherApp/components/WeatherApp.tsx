7d9e6d3af2278f6d1ac94e10e6a5b1912d4f5fd9
tomthehuman <grimmm94@gmail.com>
2021-02-02 18:46:19 -0800

    Saved longitude/latitude to state from componentDidMount Created getGeoCode function - function uses axios to call Google Geocode Api - Long/Lat in state used to obtain city/state code - City and state code updated in state Updated value for city and state in textContainer to use state

--- src/webparts/weatherApp/components/WeatherApp.tsx
+++ src/webparts/weatherApp/components/WeatherApp.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import styles from "./WeatherApp.module.scss";
 import { IWeatherAppProps } from "./IWeatherAppProps";
 import { escape } from "@microsoft/sp-lodash-subset";
+import axios from "axios";
 
 export default class WeatherApp extends React.Component<IWeatherAppProps, any> {
   constructor(props: IWeatherAppProps) {
@@ -10,35 +11,44 @@ export default class WeatherApp extends React.Component<IWeatherAppProps, any> {
       longitude: null,
       latitude: null,
       city: "Sacramento",
-      state: "CA",
+      state_code: "CA",
       temperature: "69",
       scale: "F",
     };
+    this.getGeoCode = this.getGeoCode.bind(this);
   }
 
   componentDidMount() {
-    if ("geolocation" in navigator) {
-      // use geolocation to get long and lat
+    // Set state with current coordinates
+    navigator.geolocation.getCurrentPosition((success) =>
+      this.setState(
+        {
+          longitude: success.coords.longitude,
+          latitude: success.coords.latitude,
+        },
+        this.getGeoCode
+      )
+    );
+  }
 
-      navigator.geolocation.getCurrentPosition(function (position) {
-        this.setState({
-          longitude: position.coords.longitude,
-          latitude: position.coords.latitude,
-        });
+  getGeoCode() {
+    // Add try/catch block here
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.latitude},${this.state.longitude}
+      &location_type=ROOFTOP&result_type=street_address&key=${this.props.geocode_api_key}`
+      )
+      .then((res) => {
+        this.setState(
+          {
+            city: res.data.results[0].address_components[2].long_name,
+            state_code: res.data.results[0].address_components[4].short_name,
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
       });
-      console.log(
-        "Longitude " +
-          this.state.longitude +
-          ", Latitude: " +
-          this.state.latitude
-      );
-    } else {
-      console.log("It don't work!");
-    }
-
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log("Position is: ", position);
-    });
   }
 
   public render(): React.ReactElement<IWeatherAppProps> {
@@ -46,12 +56,14 @@ export default class WeatherApp extends React.Component<IWeatherAppProps, any> {
       <div className={styles.weatherApp}>
         <div className={styles.flexContainer}>
           <div id={styles.imageContainer} className={styles.flexChild}>
-            <p>Image Goes Here</p>
+            <p>{this.props.description}</p>
           </div>
           <div id={styles.textContainer} className={styles.flexChild}>
             <div className={styles.forecastHeader}>
               <h2 className={styles.title}>Your forecast for</h2>
-              <h1 className={styles.location}>Sacramento, CA</h1>
+              <h1 className={styles.location}>
+                {this.state.city}, {this.state.state_code}
+              </h1>
             </div>
             <div>
               <h1 className={styles.temperature}>68°F</h1>
